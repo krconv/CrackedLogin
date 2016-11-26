85c7acec1129b62985f637bcf3dda4f9dfc62db4
Kodey Converse <krconverse@wpi.edu>
2016-11-25 22:43:31 -0500

    Finished implementing the authenticator

--- CrackedLogin/src/krconverse/bukkit/crackedlogin/Authenticator.java
+++ CrackedLogin/src/krconverse/bukkit/crackedlogin/Authenticator.java
@@ -10,11 +10,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -30,12 +29,12 @@ import org.bukkit.entity.Player;
  * An authentication tool for authenticating a player.
  */
 public class Authenticator {
+    private CrackedLogin plugin;
     private URI uri; // the authentication url
     private List<Player> authenticatedUsers; // players that have been
 					     // authenticated
     private HttpClient httpclient;
-    private Logger logger;
-
+    
     /**
      * Creates a new tool for authentication.
      * 
@@ -44,11 +43,11 @@ public class Authenticator {
      * @throws URISyntaxException
      *             Thrown if the given URI couldn't be parsed
      */
-    public Authenticator(String uri) throws URISyntaxException {
+    public Authenticator(String uri, CrackedLogin plugin) throws URISyntaxException {
 	this.uri = new URI(uri);
 	this.authenticatedUsers = new LinkedList<Player>();
 	this.httpclient = HttpClients.createDefault();
-	this.logger = Logger.getLogger("Minecraft");
+	this.plugin = plugin;
     }
 
     /**
@@ -61,13 +60,29 @@ public class Authenticator {
     public boolean canJoin(Player player) {
 	boolean result = false;
 	try {
-	    result = get(new BasicNameValuePair("username", player.getName()));
+	    result = get(new BasicNameValuePair("username", player.getName()), "join");
 	} catch (URISyntaxException | IOException e) {
-	    logger.log(Level.SEVERE, "Could not reach the authentication server!", e);
+	    plugin.getLogger().log(Level.SEVERE, "Could not reach the authentication server!", e);
 	}
 	return result;
     }
     
+    /**
+     * Bans a player on the authentication server.
+     * @param player The player to ban.
+     */
+    public void ban(Player player) {
+	// TODO add implementation for banning
+    }
+
+    /**
+     * Unbans a player on the authentication server.
+     * @param player The player to unban.
+     */
+    public void unban(Player player) {
+	// TODO add implementation for unbanning
+    }
+
     /**
      * Determines whether the given player is banned from the server.
      * 
@@ -78,11 +93,97 @@ public class Authenticator {
     public boolean isBanned(Player player) {
 	boolean result = false;
 	try {
-	    result = get(new BasicNameValuePair("username", player.getName()));
+	    result = get(new BasicNameValuePair("username", player.getName()), "banned");
 	} catch (URISyntaxException | IOException e) {
-	    logger.log(Level.SEVERE, "Could not reach the authentication server!", e);
+	    plugin.getLogger().log(Level.SEVERE, "Could not reach the authentication server!", e);
 	}
 	return result;
+    }
+    
+    /**
+     * Determines whether the given player is registered on the server.
+     * 
+     * @param player
+     *            The player to check.
+     * @return Whether the user is registered.
+     */
+    public boolean isRegistered(Player player) {
+	boolean result = false;
+	try {
+	    result = get(new BasicNameValuePair("username", player.getName()), "registered");
+	} catch (URISyntaxException | IOException e) {
+	    plugin.getLogger().log(Level.SEVERE, "Could not reach the authentication server!", e);
+	}
+	return result;
+    }
+
+    /**
+     * Attempts to authenticate a player.
+     * 
+     * @param player
+     *            The player to authenticate.
+     * @param password
+     *            The password to authenticate the player with.
+     * @return Whether the player was authenticated with given password.
+     */
+    public boolean authenticate(Player player, String password) {
+	boolean result = false;
+	try {
+	    result = get(new NameValuePair[] { 
+		    new BasicNameValuePair("username", player.getName()),
+		    new BasicNameValuePair("password", password) 
+		    }, "authenticate");
+	} catch (URISyntaxException | IOException e) {
+	    plugin.getLogger().log(Level.SEVERE, "Could not reach the authentication server!", e);
+	}
+	if (result) {
+	    authenticatedUsers.add(player);
+	}
+	return result;
+    }
+    
+    /**
+     * Deauthenticates a player.
+     * 
+     * @param player
+     *            The player to deauthenticate.
+     */
+    public void deauthenticate(Player player) {
+	if (authenticatedUsers.contains(player)) {
+	    authenticatedUsers.add(player);
+	}
+    }
+    
+    /**
+     * Determines whether a player is currently authenticated.
+     * @param player The player to check.
+     * @return Whether the player is currently authenticated.
+     */
+    public boolean isAuthenticated(Player player) {
+	return authenticatedUsers.contains(player);
+    }
+    
+    /**
+     * Determines whether there are any unauthenticated users in the
+     * given list of players.
+     * @param players The players to check.
+     * @return Whether any of the given players are not authenticated.
+     */
+    public boolean anyUnauthenticated(Collection<? extends Player> players) {
+	for (Player player : players) {
+	    if (!isAuthenticated(player)) {
+		return false;
+	    }
+	}
+	return true;
+    }
+
+    /**
+     * Determines whether there are any unauthenticated users on the server.
+     * @return Whether any joined players are not authenticated.
+     */
+    public boolean anyUnauthenticated() {
+	return anyUnauthenticated(plugin.getServer().getOnlinePlayers());
     }
 
     /**
@@ -90,15 +191,18 @@ public class Authenticator {
      * 
      * @param parameters
      *            The get parameters.
+     * @param query
+     *            The query string.
      * @return The response of the request.
      * @throws URISyntaxException
      *             Thrown if the given parameters are of invalid syntax.
      * @throws IOException
      *             Thrown if there is an error while making the request.
      */
-    private boolean get(List<NameValuePair> parameters) throws URISyntaxException, IOException {
+    private boolean get(NameValuePair[] parameters, String query) throws URISyntaxException, IOException {
 	// build the request
-	HttpGet get = new HttpGet(new URIBuilder(this.uri).setParameters(parameters).build());
+	HttpGet get = new HttpGet(
+		new URIBuilder(this.uri).setParameters(parameters).setParameter("q", query).build());
 	// send the request
 	HttpResponse response = httpclient.execute(get);
 	// grab and parse the content of the response
@@ -114,16 +218,16 @@ public class Authenticator {
      * 
      * @param parameter
      *            The get parameter.
+     * @param query
+     *            The query string.
      * @return The response of the request.
      * @throws URISyntaxException
      *             Thrown if the given parameters are of invalid syntax.
      * @throws IOException
      *             Thrown if there is an error while making the request.
      */
-    private boolean get(NameValuePair parameter) throws URISyntaxException, IOException {
-	List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-	parameters.add(parameter);
-	return get(parameters);
+    private boolean get(NameValuePair parameter, String query) throws URISyntaxException, IOException {
+	return get(new NameValuePair[] { parameter }, query);
     }
 
 }
--- CrackedLogin/src/krconverse/bukkit/crackedlogin/CrackedLogin.java
+++ CrackedLogin/src/krconverse/bukkit/crackedlogin/CrackedLogin.java
@@ -6,6 +6,8 @@
 package krconverse.bukkit.crackedlogin;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,14 +18,39 @@ import org.inventivetalent.packetlistener.PacketListenerAPI;
  * The CrackedLogin plugin.
  */
 public class CrackedLogin extends JavaPlugin {
-    
+    private Authenticator authenticator;
+    private PacketListener packetListener;
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.bukkit.plugin.java.JavaPlugin#onLoad()
      */
     @Override
     public void onLoad() {
 	APIManager.require(PacketListenerAPI.class, this);
+
+	// load the configuration
+	FileConfiguration config = getConfig();
+	config.options().copyDefaults(true);
+	if (!new File(getDataFolder(), "config.yml").exists()) {
+	    saveDefaultConfig();
+	    saveConfig();
+	}
+
+	// load up the authenticator
+	String authenticationURL = config.getBoolean("authentication.URL.UseHTTPS") ? "https://"
+		: "http://" + config.getString("authentication.URL.Host") + config.getString("authentication.URL.Path");
+	try {
+	    authenticator = new Authenticator(authenticationURL, this);
+	} catch (URISyntaxException e) {
+	    this.getLogger().log(Level.SEVERE, "The provided authentication URL parameters are incorrect!", e);
+	    this.setEnabled(false);
+	    return;
+	}
+	
+	// load up the packet listener
+	this.packetListener = new PacketListener(this);
     }
 
     /*
@@ -36,15 +63,23 @@ public class CrackedLogin extends JavaPlugin {
 	APIManager.initAPI(PacketListenerAPI.class);
 	
 	// register the login listener
-	//getServer().getPluginManager().registerEvents(new PlayerLoginListener(this), this);
-	
-	// load the configuration
-	FileConfiguration config = getConfig();
-	config.options().copyDefaults(true);
-	if (!new File(getDataFolder(), "config.yml").exists()) {
-		saveDefaultConfig();
-		saveConfig();
-	}
+	getServer().getPluginManager().registerEvents(new PlayerLoginListener(this), this);
+
     }
-    
+
+    /**
+     * Get the tool for authentication.
+     * @return The tool for authentication.
+     */
+    public Authenticator getAuthenticator() {
+        return authenticator;
+    }
+
+    /**
+     * Get the listener for packets between players and the server.
+     * @return The listener for packets.
+     */
+    public PacketListener getPacketListener() {
+        return packetListener;
+    }
 }
